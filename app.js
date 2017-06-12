b8633d5889db9b3dd5b49e68dfc5d8234a756c56
kanghyunjun <ke06060@naver.com>
2017-06-12 15:32:11 +0900

    검색기능

--- app.js
+++ app.js
@@ -35,11 +35,39 @@ app.use('/', require('./routes/home'));
 app.use('/users', require('./routes/users'));
 app.use('/posts', checkQuery, require('./routes/posts'));
 
-var port = process.env.PORT || 1000;
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('서버가 실행되었습니다.');
 });
 
+function countVisitors(req,res,next){
+  if(!req.cookies.count&&req.cookies['connect.sid']){
+    res.cookie('count', "", { maxAge: 3600000, httpOnly: true });
+    var now = new Date();
+    var date = now.getFullYear() +"/"+ now.getMonth() +"/"+ now.getDate();
+    if(date != req.cookies.countDate){
+      res.cookie('countDate', date, { maxAge: 86400000, httpOnly: true });
+
+      var Counter = require('./models/Counter');
+      Counter.findOne({name:"vistors"}, function (err,counter) {
+        if(err) return next();
+        if(counter===null){
+          Counter.create({name:"vistors",totalCount:1,todayCount:1,date:date});
+        } else {
+          counter.totalCount++;
+          if(counter.date == date){
+            counter.todayCount++;
+          } else {
+            counter.todayCount = 1;
+            counter.date = date;
+          }
+          counter.save();
+        }
+      });
+    }
+  }
+  return next();
+}
 
 function checkQuery(req, res, next){
   if(req.originalMethod != "GET") return next();
@@ -47,9 +75,23 @@ function checkQuery(req, res, next){
   var path = req._parsedUrl.pathname;
   var queryString = req._parsedUrl.query?req._parsedUrl.query:"";
 
-  if(queryString == req._parsedUrl.query){
-    return next();
-  } else {
-    return res.redirect(path+"?"+queryString);
-  }
+  if(queryString.indexOf("page=")<0){
+      if(queryString.length !== 0 ) queryString += "&";
+      queryString += "page=1";
+    }
+    if(queryString.indexOf("limit=")<0){
+      queryString += "&limit=10";
+    }
+    if(queryString.indexOf("searchType=")<0){
+      queryString += "&searchType=";
+    }
+    if(queryString.indexOf("searchText=")<0){
+      queryString += "&searchText=";
+    }
+
+    if(queryString == req._parsedUrl.query){
+      return next();
+    } else {
+      return res.redirect(path+"?"+queryString);
+    }
 }
